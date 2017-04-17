f6a73252ddac0a3e04937466dcf4d71da720b1f0
linlinliu <linlin.liu@ihandysoft.com>
2017-04-17 20:58:21 +0800

    init square

--- app/src/main/java/xyz/flove/square/MainActivity.java
+++ app/src/main/java/xyz/flove/square/MainActivity.java
@@ -1,13 +1,30 @@
 package xyz.flove.square;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public TextView mPlayer;
+	public TextView mStepCount;
+	public GameView mGameView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mGameView = (GameView)this.findViewById(R.id.gameView);
+		mPlayer = (TextView) this.findViewById(R.id.gamePlayer);
+		mStepCount = (TextView) this.findViewById(R.id.stepCount);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ app/src/main/java/xyz/flove/square/enums/Direction.java
@@ -0,0 +1,5 @@
+package xyz.flove.square.enums;
+
+public enum Direction {
+	WEST, WEST_NORTH, NORTH, EAST_NORTH, EAST, EAST_SORTH, SORTH, WEST_SORTH
+}
